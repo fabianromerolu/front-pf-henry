cb525166aaaeb810516057fe0c480ae895473e1f
fabianromerolu <fabianromerolu@gmail.com>
2025-10-25 18:16:52 -0500

    ajuste de ruta

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 import Home from "./home/page";
 
 
-export default async function Landing() {
+export default async function Page() {
 
   return (
     <main className="w-full h-max-screen flex items-center justify-center bg-[var(--color-violeta)]"> 
