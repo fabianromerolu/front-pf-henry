e091ed45a059da5d8bb6e6069647622332ccadb2
fabianromerolu <fabianromerolu@gmail.com>
2025-11-18 00:24:18 -0500

    empezando la dashboard del user

--- src/app/dashboard/renter/vehicles/[id]/page.tsx
+++ src/app/dashboard/renter/vehicles/[id]/page.tsx
@@ -1,7 +1,8 @@
 // src/app/dashboard/renter/vehicles/[id]/page.tsx
 "use client";
 
-import VehicleDetail from "@/components/dashboards/renter/vehicles/[id]/VehicleDetail.tsx";
+import VehicleDetail from "@/components/dashboards/renter/vehicles/[id]/VehicleDetail";
+
 
 
 export default function Page() {
--- src/components/dashboards/renter/MyProfile.tsx
+++ src/components/dashboards/renter/MyProfile.tsx
@@ -1,3 +1,4 @@
+//src/components/dashboards/renter/MyProfile.tsx
 "use client";
 
 import React, { useEffect, useState } from "react";
